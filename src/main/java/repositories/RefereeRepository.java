5cf476816ffec2c33d08e937fba47b3efbd8d9fc
Regina <regy_escalera@hotmail.com>
2018-11-22 16:12:30 +0100

    refereerepository
    refereerepository

---
+++ src/main/java/repositories/RefereeRepository.java
@@ -0,0 +1,10 @@
+
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import domain.Referee;
+
+public interface RefereeRepository extends JpaRepository<Referee, Integer> {
+
+}

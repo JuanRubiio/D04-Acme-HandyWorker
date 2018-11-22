8bc243d4f6d786fb820b3ea740a21fb987baec08
Regina <regy_escalera@hotmail.com>
2018-11-22 22:00:57 +0100

    add applications , complaint->complaints
    add applications , complaint->complaints

--- src/main/java/domain/FixUpTask.java
+++ src/main/java/domain/FixUpTask.java
@@ -108,7 +108,8 @@ public class FixUpTask extends DomainEntity {
 	//-------------RelationShip------------------
 
 	private Customer				customer;
-	private Collection<Complaint>	complaint;
+	private Collection<Complaint>	complaints;
+	private Collection<Application>	applications;
 	private Warranty				warranty;
 	private Category				category;
 
@@ -118,7 +119,7 @@ public class FixUpTask extends DomainEntity {
 
 	@Valid
 	@NotNull
-	@ManyToOne(optional = true)
+	@ManyToOne(optional = false)
 	public Customer getCustomer() {
 		return this.customer;
 	}
@@ -130,12 +131,22 @@ public class FixUpTask extends DomainEntity {
 	@NotNull
 	@Valid
 	@OneToMany(cascade = CascadeType.ALL)
-	public Collection<Complaint> getComplaint() {
-		return this.complaint;
+	public Collection<Complaint> getComplaints() {
+		return this.complaints;
 	}
 
-	public void setComplaint(final Collection<Complaint> complaint) {
-		this.complaint = complaint;
+	@Valid
+	@OneToMany(mappedBy = "fixUpTask")
+	public Collection<Application> getApplications() {
+		return this.applications;
+	}
+
+	public void setApplications(final Collection<Application> applications) {
+		this.applications = applications;
+	}
+
+	public void setComplaints(final Collection<Complaint> complaint) {
+		this.complaints = complaint;
 	}
 
 	@NotNull

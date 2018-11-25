f4b8cf7c34846d6f4f8469b34cccd30bcd216436
Regina <regy_escalera@hotmail.com>
2018-11-25 20:30:48 +0100

    add @Repository
    add @Repository

--- src/main/java/repositories/NoteRepository.java
+++ src/main/java/repositories/NoteRepository.java
@@ -1,16 +1,12 @@
 
 package repositories;
 
-import java.util.Collection;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import domain.Note;
 
+@Repository
 public interface NoteRepository extends JpaRepository<Note, Integer> {
-
-	@Query("select n from Note n where n.report.referee.id=?1")
-	Collection<Note> getNotesByReferee(Integer refereeId);
 
 }

4b6087a26f9fabb9d7e6bdf011e7ce0abb42d2b1
Regina <regy_escalera@hotmail.com>
2018-11-25 20:32:39 +0100

    refereeService
    refereeService

---
+++ src/main/java/services/RefereeService.java
@@ -0,0 +1,63 @@
+
+package services;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import repositories.RefereeRepository;
+import security.Authority;
+import security.UserAccount;
+import domain.Referee;
+
+@Service
+@Transactional
+public class RefereeService {
+
+	@Autowired
+	private RefereeRepository	refereeRepository;
+
+
+	public Referee create() {
+		Referee res;
+		res = new Referee();
+		final UserAccount userAccount = new UserAccount();
+		final List<Authority> authorities = new ArrayList<Authority>();
+		final Authority authority = new Authority();
+		authority.setAuthority(Authority.REFEREE);
+		authorities.add(authority);
+		userAccount.setAuthorities(authorities);
+		return res;
+	}
+
+	public Collection<Referee> findAll() {
+		Collection<Referee> res;
+		res = this.refereeRepository.findAll();
+		Assert.notNull(res);
+		return res;
+	}
+
+	public Referee findOne(final Integer refereeId) {
+		Referee res;
+		Assert.notNull(refereeId);
+		res = this.refereeRepository.findOne(refereeId);
+		Assert.notNull(res);
+		return res;
+	}
+
+	public void delete(final Referee referee) {
+		Assert.notNull(referee);
+		this.refereeRepository.delete(referee);
+	}
+
+	public void save(final Referee referee) {
+		Assert.notNull(referee);
+		this.refereeRepository.save(referee);
+	}
+}
